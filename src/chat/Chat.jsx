71308e63877890cfbd44a51751da99603bd7957a
li13 <157872969@qq.com>
2024-05-30 22:32:35 +0800

    Emoji

--- src/chat/Chat.jsx
+++ src/chat/Chat.jsx
@@ -1,8 +1,20 @@
 import React, { useState } from "react";
+import EmojiPicker from "emoji-picker-react";
 import "./chat.css";
 const Chat = () => {
   // 输入框文本
   const [Text, setText] = useState("");
+  // 控制表情框是否展示
+  const [Open, setOpen] = useState(false);
+  const handleInput = (e) => {
+    setText(e.target.value);
+  };
+  const handleEmojiClick = (e) => {
+    setText((pre) => pre + e.emoji);
+    setOpen(false);
+  };
+
+  console.log(Text);
   return (
     <div className="chat">
       <div className="top">
@@ -26,8 +38,21 @@ const Chat = () => {
           <img src="./more.png" alt="" />
           <img src="./more.png" alt="" />
         </div>
-        <input type="text" placeholder="Send a Message" />
-        <img className="emoji" src="./emoji.png" alt="" />
+        <input
+          type="text"
+          placeholder="Send a Message"
+          value={Text}
+          onChange={handleInput}
+        />
+        <img
+          className="emoji"
+          src="./emoji.png"
+          alt=""
+          onClick={() => {
+            setOpen(!Open);
+          }}
+        />
+        <EmojiPicker open={Open} onEmojiClick={handleEmojiClick}></EmojiPicker>
         <button>Send</button>
       </div>
     </div>
